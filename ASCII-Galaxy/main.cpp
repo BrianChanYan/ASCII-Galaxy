1848b4880e386e674f865118b03993ee059041bb
Brian <brianchan020516@gmail.com>
2026-02-26 15:14:34 +0800

    Initial Commit

---
+++ ASCII-Galaxy/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ASCII-Galaxy
+//
+//  Created by Brian Chan on 2026/2/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
